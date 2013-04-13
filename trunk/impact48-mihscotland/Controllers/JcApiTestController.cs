faee366e840f13e526948824b38faa8626b0e456
barry@endzone.co.uk <barry@endzone.co.uk@870c6c47-a5a3-e8e7-046f-ac7f533020b0>
2013-04-13 09:24:52 +0000

    MVC project Test JustGiving API integration

---
+++ trunk/impact48-mihscotland/Controllers/JcApiTestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JustGiving.Api.Sdk;
+
+namespace impact48_mihscotland.Controllers
+{
+    public class JcApiTestController : Controller
+    {
+        //
+        // GET: /ApiTest/
+
+        public ActionResult Index()
+        {
+            string key = ConfigurationManager.AppSettings["JustGiving.Api.Key"];
+            var client = new JustGivingClient(key);
+            var page = client.Charity.Retrieve(185656);
+
+            return View();
+        }
+
+    }
+}
