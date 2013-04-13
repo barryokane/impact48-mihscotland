8054f5424f6ba51fa8ec9037897767311f9494fb
vladimirs.surajevs@gmail.com <vladimirs.surajevs@gmail.com@870c6c47-a5a3-e8e7-046f-ac7f533020b0>
2013-04-13 20:10:29 +0000

    + Initial commit of "Add Youtube Video"

---
+++ impact48-mihscotland-youtube/MiHs/Controls/AddYoutubeVideo.ascx
@@ -0,0 +1,19 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AddYoutubeVideo.ascx.cs" Inherits="MiHs.Controls.AddYoutubeVideo" %>
+
+<p>
+    <asp:Label ID="UrlLabel" runat="server" AssociatedControlID="Url" Text="Url"/><br/>
+    <asp:TextBox ID="Url" runat="server"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="UrlRequiredValidator" runat="server" ControlToValidate="Url" 
+        ErrorMessage="Url required" ForeColor="Red" Display="Dynamic"/>
+    <asp:CustomValidator ID="UrlValidator" runat="server" ControlToValidate="Url" ForeColor="Red" 
+        OnServerValidate="ValidateUrl" ErrorMessage="Invalid Url" Display="Dynamic"/>
+</p>
+
+<p>
+    <asp:Label runat="server" AssociatedControlID="Comment" Text="Comment"/><br/>   
+    <asp:TextBox ID="Comment" runat="server"></asp:TextBox>
+</p>
+
+<p>
+    <asp:Button ID="Add" runat="server" CausesValidation="True" Text="Add" OnClick="Add_Click"/>
+</p>
---
+++ impact48-mihscotland-youtube/MiHs/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using Google.YouTube;
+using MiHs.Controls;
+using MiHs.DAL;
+using MiHs.DAL.DomainObjects;
+
+namespace MiHs
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private readonly MihDbContext _mihDbContext;
+
+        public Default()
+        {
+            _mihDbContext = new MihDbContext();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!IsPostBack)
+            {
+                YoutubeVideos.DataBind();
+            }
+        }
+
+        protected void AddYoutubeVideoControl_AddingYoutubeVideo(object sender, AddYoutubeVideo.AddYoutubeVideoEventArgs e)
+        {
+            var youtubeVideoIdQueryStringParamaterName = ConfigurationManager.AppSettings["Youtube.VideoIdQueryStringParamametrName"];
+            var youtubeVideoReference = HttpUtility.ParseQueryString(e.Uri.Query).Get(youtubeVideoIdQueryStringParamaterName);
+            var youtubeVideo = new YoutubeVideo
+                {
+                    YoutubeVideoReference = youtubeVideoReference,
+                    Comment = e.Comment
+                };
+
+            _mihDbContext.YoutubeVideos.Add(youtubeVideo);
+            _mihDbContext.SaveChanges();
+
+            var developerKey = ConfigurationManager.AppSettings["Youtube.DeveloperKey"];
+            var appName = ConfigurationManager.AppSettings["Youtube.ApplicationName"];
+            var userName = ConfigurationManager.AppSettings["Youtube.Username"];
+            var password = ConfigurationManager.AppSettings["Youtube.Password"];
+            var youTubeRequestSettings = new YouTubeRequestSettings(appName, developerKey, userName, password);
+            var youTubeRequest = new YouTubeRequest(youTubeRequestSettings);
+            var user = ConfigurationManager.AppSettings["Youtube.User"];
+            var playlistsFeed = youTubeRequest.GetPlaylistsFeed(user);
+            var playlistTitle = ConfigurationManager.AppSettings["Youtube.PlaylistTitle"];
+            var playlist = playlistsFeed.Entries.FirstOrDefault(p => p.Title == playlistTitle);
+            var playListMember = new PlayListMember { VideoId = youtubeVideo.YoutubeVideoReference };
+
+            youTubeRequest.AddToPlaylist(playlist, playListMember);
+
+            YoutubeVideos.DataBind();
+        }
+
+        protected void YoutubeVideos_DataBinding(object sender, EventArgs e)
+        {
+            var repeater = sender as Repeater;
+            if (repeater != null) 
+                repeater.DataSource = _mihDbContext.YoutubeVideos.ToList();
+        }
+    }
+}
